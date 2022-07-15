a574223a3ff08a22ccfa68f840ee97b9ffffef0b
aaloksikand <aalok.sikand@gmail.com>
2022-07-14 18:40:44 -0700

    Reinstalled package.json as suggested by AskBCS

--- server.js
+++ server.js
@@ -1,82 +1,78 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 //db.json file on back end that will be used to store and retrieve notes using the fs module
-const fs = require('fs');
+const fs = require("fs");
 //helper method for generating unique ids
-const uuid = require('uuid');
+const uuid = require("uuid");
 
 //db.json file on back end that will be used to store and retrieve notes using the fs module
-const notesData = require ('./db/db.json');
-
+const notesData = require("./db/db.json");
 
 const PORT = 3001;
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 //1.  GET /notes should return the notes.html file
-app.get('/notes', (req, res) => {
-  res.sendFile(__dirname + '/public/notes.html');
+app.get("/notes", (req, res) => {
+  res.sendFile(__dirname + "/public/notes.html");
 });
 //2.  GET * should return the index.html file
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html')
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
-
 
 //CREATE API ROUTES:
 //1.  GET /api/notes should read the db.json file and return all saved notes as JSON
-app.get('/api/notes', (req,res) => res.json(notesData));
+app.get("/api/notes", (req, res) => res.json(notesData));
 
-//2.  POST /api/notes should receive a new note to save on the request body, 
+//2.  POST /api/notes should receive a new note to save on the request body,
 //and then return the new note to the client.
-app.post('api/notes', (req, res) => {
-console.info(`${req.method} request received to add a note`);
-//Destructuring assignment for the items in req.body
-const { title, text } = req.body;
-//If all the required properties are present
-if (title && text) {
-  const newNote = {
-    title,
-    text,
-    note_id: uuid(),
-  };
-//Obtain existing notes
-fs.readFile('./db/db.json','utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-  }
-  else {
-    const parsedNotes = JSON.parse(data);
+app.post("api/notes", (req, res) => {
+  console.log(`${req.method} request received to add a note`);
+  //Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+  //If all the required properties are present
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+    //Obtain existing notes
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(data);
 
-    //Add a new note to db.json file
-    parsedNotes.push(newNote);
-  fs.writeFile(
-    './db/db.json',
-    JSON.stringify(parsedNotes, null, 4),
-    (writeErr) =>
-    writeErr
-    ? console.error(writeErr)
-    :console.info('Successfully updated notes!')
-  )
-  }
-})
-const response = {
-  status: "success",
-  body: newNote,
-};
+        //Add a new note to db.json file
+        parsedNotes.push(newNote);
+        fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info("Successfully updated notes!")
+        );
+      }
+    });
+    const response = {
+      status: "success",
+      body: newNote,
+    };
 
-console.log(response);
-res.status(201).json(response);
-} else {
-res.status(500).json("Error in posting new note");
-}
-}
-);
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("Error in posting new note");
+  }
+});
 //BONUS:
 //1. DELETE api/notes/:id
 //you'll need to read all notes from the db.json file,
@@ -86,5 +82,3 @@ res.status(500).json("Error in posting new note");
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
-
-
