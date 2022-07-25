6029caa75df647d96d979fff435d4af04a6e0f8b
aaloksikand <aalok.sikand@gmail.com>
2022-07-25 14:45:48 -0700

    updated PORT

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { v4: uuid } = require("uuid"); //destructured v4 from the package and ren
 //db.json file on back end that will be used to store and retrieve notes using the fs module
 const notesData = require("./db/db.json");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
