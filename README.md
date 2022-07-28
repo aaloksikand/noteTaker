b423a08666d67259faabdf756a1973a900533573
aaloksikand <aalok.sikand@gmail.com>
2022-07-28 09:22:09 -0700

    Readme file created

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # noteTaker
+
+![License](https://img.shields.io/static/v1?label=license&message=mit&color=success)
+
+## Table of Contents
+
+-   [Description](#description)
+-   [Installation Instructions]
+(#installation-instructions)
+-   [Usage] (#usage)
+-   [License] (#license)
+-   [Questions and Contributions](#questions-and-contributions)
+
+## Description
+
+Note Taker is an application that provides a user with functionality to add title and text of notes and delete, save and update said notes.
+
+Note Taker uses state-of-the-art full-stack technology including but not limited to:
+1. Node, 
+2. Express, 
+3. Path Package 
+4. FS Package
+5. UUID package 
+6. Get Routes
+7. Post Routes
+8.  Bootstrap
+
+
+## Installation Instructions
+
+1.  Goto the following link and clone the repository:
+2.  Once cloned, in your terminal of choice (i.e. VSCode, Git Bash etc.), type in "Run NPM I" to install the following Node.js dependecies:
+    A. Express
+    B. FS
+    C. Path
+    D. UUID
+3.  Please fun "npm run seed" to seed the data.
+3.  After installing all dependencies and seeding the data, please type "npm run start" from your terminal of choice to spin up the server.
+4.  Please goto http://localhost:3001 to access the Note Taker Homepage.
+
+## Usage
+1.  Please goto http://localhost:3001 to access the Note Homepage.
+2.  The left-side panel provides pre-existing notes.
+3.  The center page allows you to input Note Title and Note Text.
+4.  The disk and trash buttons allow you to save and delete notes.
+
+## License
+
+The following is the license used for this project <br> https://choosealicense.com/licenses/mit
+
+## Questions And Contributions
+
+If you have any questions about our project you can find us on github at  [aaloksikand] (https://github.com/aaloksikand) or you can email us at  aalok.sikand@gmail.com respectively.
+
+## Links
+
+Deployed Project:  https://github.com/james91055/ReviewFlix
+
+Heroku Link: https://boiling-plateau-87824.herokuapp.com/
+
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+//requiring the express package
 const express = require("express");
+//requiring path package
 const path = require("path");
 //db.json file on back end that will be used to store and retrieve notes using the fs module
 const fs = require("fs");
@@ -8,8 +10,10 @@ const { v4: uuid } = require("uuid"); //destructured v4 from the package and ren
 //db.json file on back end that will be used to store and retrieve notes using the fs module
 const notesData = require("./db/db.json");
 
+//heroku port or 3001
 const PORT = process.env.PORT || 3001;
 
+//app is the express function
 const app = express();
 
 app.use(express.json());
@@ -17,35 +21,35 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + "/public"));
 
-//1.  GET /notes should return the notes.html file
+//GET /notes should return the notes.html file
 app.get("/notes", (req, res) => {
   res.sendFile(__dirname + "/public/notes.html");
 });
-//2.  GET * should return the index.html file
+//GET * should return the index.html file
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-//CREATE API ROUTES:
-//1.  GET /api/notes should read the db.json file and return all saved notes as JSON
+//GET /api/notes should read the db.json file and return all saved notes as JSON
 app.get(
   "/api/notes",
   (
     req,
-    res //res.json(notesData));
+    res
   ) =>
+  //reading the json file and sending it unless there is an error.
     fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
       if (err) {
         console.error(err);
       } else {
-      res.send(data); //I don't need to use res.json because 
+      res.send(data); 
       }
     })
 );
 
-//2.  POST /api/notes should receive a new note to save on the request body,
-//and then return the new note to the client.
+//POST /api/notes should receive a new note to save on the request body, and then return the new note to the client.
 app.post("/api/notes", (req, res) => {
+  //Consoling the Post method
   console.log(`${req.method} request received to add a note`);
   //Destructuring assignment for the items in req.body
   const { title, text } = req.body;
@@ -86,12 +90,6 @@ app.post("/api/notes", (req, res) => {
     res.status(500).json("Error in posting new note");
   }
 });
-//BONUS:
-//1. DELETE api/notes/:id
-//you'll need to read all notes from the db.json file,
-//remove the note with the given id property, and then rewrite the notes
-//to the db.json file
-
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
